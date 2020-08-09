9c02cef9af0049cbb3fb0df1c3d8d0163eedcd53
tesdy14 <tesdy14@hotmail.fr>
2020-08-09 18:23:15 +0200

    ajout de l'entité Property

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,7 +3,7 @@
 
 namespace App\Controller;
 
-
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +12,25 @@ class PropertyController extends AbstractController
 {
 
     /**
+     * @var PropertyRepository
+     */
+    private $pRepository;
+
+    public function __construct(PropertyRepository $propertyRepository)
+    {
+        $this->pRepository = $propertyRepository;
+
+    }
+
+    /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
     public function index(): Response
     {
+        dump($this->pRepository);
+
+
         return $this->render('property/index.html.twig', [
             'current_menu' =>  'properties',
         ]);
